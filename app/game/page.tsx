5981f20057eec1492eb4b270a368bf75c90daf73
EMobilio <eymobilio@gmail.com>
2025-04-23 23:06:47 -0400

    added outline for pages

---
+++ app/game/page.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function GamePage() {
+    const [rounds, setRounds] = useState([]);
+    const [roundNumber, setRoundNumber] = useState(0);
+    const [score, setScore] = useState(0);
+    const [answer, setAnswer] = useState("");
+    const [image, setImage] = useState("");
+
+    return (
+        <main>
+
+        </main>
+    );
+}
