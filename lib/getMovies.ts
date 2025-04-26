df7ccf34594244fce9a700b62573e282016c93d7
Thomas Nguyen <144154873+Th0masNguyen@users.noreply.github.com>
2025-04-26 19:31:34 -0400

    comments to lib, expanded movie pool

--- lib/getMovies.ts
+++ lib/getMovies.ts
@@ -1,14 +1,15 @@
-//gets the 200 most popular movies of all time as a base pool from which the game is played
-
+//written by Thomas Nguyen (nguyen04@bu.edu)
+//gets the n most popular movies of all time as a base pool from which the game is played
 "use server";
 
 import { Movie } from "@/types";
 
-export default async function getMovies(): Promise<Movie[]> {
+export default async function getMovies(n: number = 300): Promise<Movie[]> {
   const movies: Movie[] = [];
-
+  const numFetches = Math.floor(n / 20);
+  //api returns 20 movies at a time
   try {
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= numFetches; i++) {
       const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${i}&sort_by=vote_count.desc`, {
         method: 'GET',
         headers: {
