8205eabf7a50891dba2f8f0a0c14ee3cb96b6ae4
coltonc1 <coltonc@bu.edu>
2025-04-26 19:54:04 -0400

    Updated navbar text color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@
 import Link from "next/link";
 
 export default function Header() {
-    const linkStyling = "p-1 m-2 text-xl hover:underline";
+    const linkStyling = "p-1 m-2 text-xl hover:underline text-white";
     return (
       <header className="flex justify-between items-center h-20">
         <nav className="p-2 m-4">
