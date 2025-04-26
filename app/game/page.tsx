7da1a157b8678926d71ed9aeee4c4e5e376086c0
coltonc1 <coltonc@bu.edu>
2025-04-26 18:33:11 -0400

    Fixed round number

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -161,7 +161,7 @@ export default function GamePage() {
     // render the main game content
     return (
         <main className={"flex flex-col items-center pt-30 w-full h-full text-[#5863F8]"}>
-            <GameStateDisplay roundNumber={roundNumber} score={score}/>
+            <GameStateDisplay roundNumber={roundNumber + 1} score={score}/>
 
             <img src={image} alt={"Round image"} className={"w-[80%] md:w-[50%] h-auto mb-10 border-2 border-[#5863F8]"} />
 
