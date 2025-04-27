0afc5129ee73c277cb0b0e4558c216f4b7f41a24
coltonc1 <coltonc@bu.edu>
2025-04-26 21:04:20 -0400

    Styling leaderboard

--- components/DisplayScores.tsx
+++ components/DisplayScores.tsx
@@ -10,10 +10,10 @@ export default async function DisplayScores() {
     const scores = await getAllScores();
 
     return(
-        <table>
+        <table className="table-auto border">
             <tr>
-                <th>Name</th>
-                <th>Score</th>
+                <th className="px-10 py-2 text-4xl border">Name</th>
+                <th className="px-10 py-2 text-4xl border">Score</th>
             </tr>
             {scores.map((s) =>
                 <tr>
