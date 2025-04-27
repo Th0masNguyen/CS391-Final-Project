64eec472af26bace69d88a8b4944e66565071173
coltonc1 <coltonc@bu.edu>
2025-04-26 20:40:10 -0400

    Working on getting scores for leaderboard

--- lib/getAllUserScores.ts
+++ lib/getAllUserScores.ts
@@ -1,9 +1,15 @@
+/** 
+ * Created by Colton Connolly (coltonc@bu.edu)
+ * Get's all user and score pairs for leaderboard
+*/
+
+
 import { ScoreProps } from "@/types";
 import getCollection, { ALIAS_COLLECTION } from "@/db";
 
 export default async function getAllPosts(): Promise<ScoreProps[]> {
   const postsCollection = await getCollection(ALIAS_COLLECTION);
-  const data = await postsCollection.find().toArray();
+  const data = await postsCollection.find().sort({"score": -1}).toArray();
 
   const scores: ScoreProps[] = data.map((p) => ({
     name: p.name,
