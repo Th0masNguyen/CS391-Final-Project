4d24a5af33c342f88ec07e8772626bad91d50548
coltonc1 <coltonc@bu.edu>
2025-04-26 20:43:44 -0400

    Renamed file

--- app/leaderboard/leaderboard.tsx
+++
@@ -1,5 +0,0 @@
-
-/** 
- * Created by Colton Connolly (coltonc@bu.edu)
- * Displays leaderboard scores
-*/
---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,13 @@
+
+/** 
+ * Created by Colton Connolly (coltonc@bu.edu)
+ * Displays leaderboard scores
+*/
+
+export default function Leaderboard() {
+    return(
+        <>
+        <h1>Leaderboard</h1>
+        </>
+    )
+}
