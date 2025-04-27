bcf63bf47da767b660baa99c94f38a4562fd65fa
Thomas Nguyen <144154873+Th0masNguyen@users.noreply.github.com>
2025-04-26 20:01:31 -0400

    added instructions, changed home styling

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,14 @@ export default function Home() {
 
     // render the home page with the UserForm component for the user to enter a username and start the game
     return (
-        <main className={"flex flex-col pt-60 items-center w-full h-full text-[#5863F8]"}>
-            <h1 className={"text-6xl font-semibold m-10"}>FlickPic</h1>
+        <main className="flex flex-col items-center justify-center w-full h-full text-[#5863F8]">
+            <h1 className="text-6xl font-semibold mb-10">FlickPic</h1>
             <UserForm username={username} setUsername={setUsername} />
+            <ul className="mt-5 text-center space-y-2 text-xl">
+                <li>Try to guess which movie an image belongs to.</li>
+                <li>You have 10 rounds to get as many correct as you can.</li>
+                <li>See if you can get to the top of the leaderboard!</li>
+            </ul>
         </main>
     );
 }
